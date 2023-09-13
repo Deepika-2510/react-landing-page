c3fb527a6195a75f12bf7df13f21ea8bf5d630ec
Deepika-2510 <optimistdeepika@gmail.com>
2023-09-13 19:05:22 +0530

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,21 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Getting started is <br className="sm:block hidden" /> quick and easy.
+      </h2>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
